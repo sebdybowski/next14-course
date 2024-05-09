7b3ba605e98ff86606e61f1ff371ee7d1770bb56
Seb Dybowski <dybowski.seb@gmail.com>
2024-05-10 00:26:22 +0200

    WIP

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,8 @@ function HomepageHeader() {
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <Heading as="h1" className="hero__title">
-          {siteConfig.title}
+          Work in progress!
+	  {siteConfig.title}
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
